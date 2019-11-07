73535d04d2ca280b38c009be0394dc41a934a107
Enoch Riese <enoch.riese@gmail.com>
2019-11-06 21:09:45 -0600

    oauth but no token storage

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express();
+const path = require('path');
+const {requestAuth, acceptAuth} = require('./oauth.js')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/index.html'))
 });
+app.get('/settings', (req, res) => {
+	res.send('made it to settings');
+})
+
+app.get('/auth', requestAuth);
+app.get('/accept-auth', acceptAuth);
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
